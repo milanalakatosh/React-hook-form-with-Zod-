702666a5eb3dd56c880e0d4121ce75aff6868b45
Milana <milanammk@gmail.com>
2023-02-24 11:08:01 +0200

    installation, Form

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="max-w-xl mx-auto w-full">
+      <div className="flex justify-center my-12">
+        <div className="w-full lg:w-11/12 bg-white p-5 rounded-lg shadow-xl">
+          <h3 className="pt-4 text-2xl text-center font-bold">
+            Create New Account
+          </h3>
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
